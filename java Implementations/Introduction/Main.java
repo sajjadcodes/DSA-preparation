50777bf71b2709e48e1fbfdd7b9f5efeb7d3a265
sajjadcodes <sakraan01@gmail.com>
2022-06-26 12:34:35 +0500

    introduction

---
+++ java Implementations/Introduction/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // if you are creating class the convention name should be Capital. Also every project must have main
+    // If file name is Main then. And Main class must be public class. So it will be accessible for public
+    // By convention main function. and also it is the starting point
+    public static void main(String[] args) {
+        System.out.print("Hello World.!");
+    }
+}
